5ff2b0ad5575d6f6ec4d1e3e490b67b03030ce1f
btkostner <btkostner@users.noreply.github.com>
2020-11-21 00:23:38 +0000

    deploy: 748af3acbf242ff01cdd905de24f9672d9e636e1

--- _nuxt/static/1605917953/tags/dev/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1605917953",layout:"default",error:null,serverRendered:a,routePath:"\u002Ftags\u002Fdev",config:{content:{dbHash:"1dab95a5"}},colorMode:{preference:b,value:b,unknown:a,forced:false}}}(true,"system"));
---
+++ _nuxt/static/1605918211/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1605918211",layout:"default",error:null,serverRendered:a,routePath:"\u002F",config:{content:{dbHash:"3e1547da"}},colorMode:{preference:b,value:b,unknown:a,forced:false}}}(true,"system"));
