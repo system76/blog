251711c9e729de89b0864fc1fd573c57f7c1ad3c
btkostner <btkostner@users.noreply.github.com>
2021-06-25 05:37:09 +0000

    deploy: 169643237716c50d5e8a87ad3403eb9f80c23b59

---
+++ _nuxt/static/1624599425/post/behind-the-scenes-of-system76-industrial-design/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fblog\u002F_nuxt\u002Fstatic\u002F1624599425",layout:"default",error:a,serverRendered:true,routePath:"\u002Fpost\u002Fbehind-the-scenes-of-system76-industrial-design",config:{_app:{basePath:"\u002Fblog\u002F",assetsPath:"\u002Fblog\u002F_nuxt\u002F",cdnURL:a}}}}(null));
