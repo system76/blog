2511fa2ee583374c8eda288c2d10a2bbd484b8a5
btkostner <btkostner@users.noreply.github.com>
2021-06-25 22:45:20 +0000

    deploy: dd857e95c18d831029f0a0bc5385f34deca3c952

--- _nuxt/static/1624648514/post/gaming-on-linux-guide-to-graphics/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624648514",layout:"default",error:a,serverRendered:true,routePath:"\u002Fpost\u002Fgaming-on-linux-guide-to-graphics",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
