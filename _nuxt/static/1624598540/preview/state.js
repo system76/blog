46cba09dabac07cdf63293375cf390740009e1c4
btkostner <btkostner@users.noreply.github.com>
2021-06-25 05:22:23 +0000

    deploy: 51306d7e9f1d18422944bd2cf44db5a95422719e

---
+++ _nuxt/static/1624598540/preview/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fy\u002F_nuxt\u002Fstatic\u002F1624598540",layout:"default",error:a,serverRendered:true,routePath:"\u002Fpreview",config:{_app:{basePath:"\u002Fy\u002F",assetsPath:"\u002Fy\u002F_nuxt\u002F",cdnURL:a}}}}(null));
