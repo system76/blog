d5960b017a7f2967c46fd6d6599929dc7624114d
btkostner <btkostner@users.noreply.github.com>
2021-06-25 05:25:05 +0000

    deploy: 80682e3645cf847b7a42ba9b2da974e59bffd7f0

---
+++ _nuxt/static/1624598701/post/uypp-ben-ruels-garage-gauypp-ben-ruels-garage-gardenrden/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fy\u002F_nuxt\u002Fstatic\u002F1624598701",layout:"default",error:a,serverRendered:true,routePath:"\u002Fpost\u002Fuypp-ben-ruels-garage-gauypp-ben-ruels-garage-gardenrden",config:{_app:{basePath:"\u002Fy\u002F",assetsPath:"\u002Fy\u002F_nuxt\u002F",cdnURL:a}}}}(null));
