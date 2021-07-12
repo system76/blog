f95bd3428b6b86f1c49879458682570c48fe1fe4
btkostner <btkostner@users.noreply.github.com>
2021-07-12 20:33:41 +0000

    deploy: f278b01ab0daa333226d60185f58922fa6a53e19

--- _nuxt/static/1624662924/post/developing-games-on-linux-an-interview-with/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624662924",layout:"default",error:a,serverRendered:true,routePath:"\u002Fpost\u002Fdeveloping-games-on-linux-an-interview-with",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
