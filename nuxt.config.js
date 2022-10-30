d0656f3c6e49fa5f7f2e10836e826ea8a0b3f87c
jasenmichael <jasen@jasenmichael.com>
2022-10-30 15:34:06 -0500

    chore: format

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,16 +2,15 @@ import { createFeed, updateRssFile } from './plugins/generateRss.js' // eslint-d
 
 import { apiEndpoint } from './sm.json'
 const prismicApiUrl = apiEndpoint
-
-const HOST =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:3000'
-    : 'https://blog.system76.com'
+const HOST = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://blog.system76.com'
 
 export default {
   target: 'static',
 
-  components: ['~/components', { path: '~/slices', extensions: ['vue'] }],
+  components: [
+    '~/components',
+    { path: '~/slices', extensions: ['vue'] }
+  ],
 
   telemtry: true,
 
@@ -25,8 +24,7 @@ export default {
     description: 'Official System76 Blog',
     color: '#4e4540',
 
-    titleTemplate: title =>
-      title !== '' ? `${title} - System76 Blog` : 'System76 Blog',
+    titleTemplate: title => (title !== '') ? `${title} - System76 Blog` : 'System76 Blog',
 
     htmlAttrs: {
       lang: 'en',
@@ -40,56 +38,26 @@ export default {
 
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
 
-      {
-        hid: 'description',
-        name: 'description',
-        content: 'Official System76 Blog'
-      },
+      { hid: 'description', name: 'description', content: 'Official System76 Blog' },
 
-      {
-        hid: 'og:site_name',
-        property: 'og:site_name',
-        content: 'System76 Blog'
-      },
+      { hid: 'og:site_name', property: 'og:site_name', content: 'System76 Blog' },
       { hid: 'og:title', property: 'og:title', content: 'System76 Blog' },
-      {
-        hid: 'og:description',
-        property: 'og:description',
-        content: 'Official System76 Blog'
-      },
-      {
-        hid: 'og:image',
-        property: 'og:image',
-        content: `${HOST}/images/social.jpg`
-      },
-      {
-        hid: 'og:image:alt',
-        property: 'og:image:alt',
-        content: 'The Blog of System76'
-      },
+      { hid: 'og:description', property: 'og:description', content: 'Official System76 Blog' },
+      { hid: 'og:image', property: 'og:image', content: `${HOST}/images/social.jpg` },
+      { hid: 'og:image:alt', property: 'og:image:alt', content: 'The Blog of System76' },
       { hid: 'og:image:width', property: 'og:image:width', content: 500 },
       { hid: 'og:image:height', property: 'og:image:height', content: 1000 },
 
       { hid: 'twitter:title', name: 'twitter:title', content: 'System76 Blog' },
-      {
-        hid: 'twitter:description',
-        name: 'twitter:description',
-        content: 'Official System76 Blog'
-      },
+      { hid: 'twitter:description', name: 'twitter:description', content: 'Official System76 Blog' },
       { hid: 'twitter:site', name: 'twitter:site', content: '@system76' },
-      {
-        hid: 'twitter:card',
-        name: 'twitter:card',
-        content: 'summary_large_image'
-      },
-      {
-        hid: 'twitter:image:src',
-        name: 'twitter:image:src',
-        content: `${HOST}/images/social.jpg`
-      }
+      { hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
+      { hid: 'twitter:image:src', name: 'twitter:image:src', content: `${HOST}/images/social.jpg` }
     ],
 
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
 
     script: [
       {
@@ -99,8 +67,7 @@ export default {
         src: 'https://plausible.io/js/plausible.outbound-links.js'
       },
       {
-        innerHTML:
-          'window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }'
+        innerHTML: 'window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }'
       }
     ],
 
@@ -114,11 +81,22 @@ export default {
     '~/assets/styles/main.css'
   ],
 
-  plugins: ['~/plugins/components', '~/plugins/font-awesome'],
+  plugins: [
+    '~/plugins/components',
+    '~/plugins/font-awesome'
+  ],
 
-  buildModules: ['@nuxt/image', '@nuxtjs/tailwindcss'],
+  buildModules: [
+    '@nuxt/image',
+    '@nuxtjs/tailwindcss'
+  ],
 
-  modules: ['nuxt-sm', '@nuxtjs/prismic', '@nuxtjs/sitemap', '@nuxtjs/feed'],
+  modules: [
+    'nuxt-sm',
+    '@nuxtjs/prismic',
+    '@nuxtjs/sitemap',
+    '@nuxtjs/feed'
+  ],
 
   loading: {
     color: '#6ACAD8',
@@ -169,10 +147,7 @@ export default {
   ],
 
   storybook: {
-    stories: [
-      '~/slices/**/*.stories.js',
-      '~/.slicemachine/assets/slices/**/*.stories.js'
-    ]
+    stories: ['~/slices/**/*.stories.js', '~/.slicemachine/assets/slices/**/*.stories.js']
   },
 
   ignore: ['**/*.stories.js'],
