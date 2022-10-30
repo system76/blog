ebee66d2c77c2ec50fb1bae0e2ceff89d85b7f8c
jasenmichael <jasen@jasenmichael.com>
2022-10-30 14:43:11 -0500

    chore: add debug logs

--- plugins/generateRss.js
+++ plugins/generateRss.js
@@ -49,13 +49,13 @@ const createFeed = async (feed, HOST) => {
 }
 
 const updateRssFile = () => {
-  // const rssFeedPath = '../dist/rss.xml'
-  // const rssFeed = fs.readFileSync(rssFeedPath, 'utf8')
-  // const rssFeedReplaced = rssFeed.replace(
-  //   '?>',
-  //   '?>\r\n<?xml-stylesheet href="/rss.xsl" type="text/xsl"?>'
-  // )
-  // fs.writeFileSync(rssFeedPath, rssFeedReplaced)
+  const rssFeedPath = 'dist/rss.xml'
+  const rssFeed = fs.readFileSync(rssFeedPath, 'utf8')
+  const rssFeedReplaced = rssFeed.replace(
+    '?>',
+    '?>\r\n<?xml-stylesheet href="/rss.xsl" type="text/xsl"?>'
+  )
+  fs.writeFileSync(rssFeedPath, rssFeedReplaced)
 }
 
 module.exports = {
