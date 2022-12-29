9a645020ed69ed88c72025eaa4376fe375b496fb
jasenmichael <jasenmichael@users.noreply.github.com>
2022-12-29 16:54:16 +0000

    deploy: e4791020cd9f5b8ecf9313504f306f50bc89d171

---
+++ _nuxt/static/1672332850/tags/Case Study/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1672332850",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftags\u002FCase%20Study",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
