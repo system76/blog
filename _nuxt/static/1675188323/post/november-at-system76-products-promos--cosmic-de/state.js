6708c38908d97284623e84a707b5d633dbade608
jasenmichael <jasenmichael@users.noreply.github.com>
2023-02-15 18:49:52 +0000

    deploy: beb94cd3b75d5a534b9d85229b361f49665a0dfb

--- _nuxt/static/1675188323/post/november-at-system76-products-promos--cosmic-de/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1675188323",layout:"default",error:a,serverRendered:true,routePath:"\u002Fpost\u002Fnovember-at-system76-products-promos--cosmic-de",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
