f7f6f5a4355c03f2b9f1dafc6bdc6364cac7d4f1
burden <burden@users.noreply.github.com>
2023-04-07 13:32:59 +0000

    deploy: 61fb3d04d1ac9fca02a40e2c02f222123664f9a9

---
+++ _nuxt/static/1680874372/post/open-up-contributions-and-collaborations/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1680874372",layout:"default",error:a,serverRendered:true,routePath:"\u002Fpost\u002Fopen-up-contributions-and-collaborations",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
