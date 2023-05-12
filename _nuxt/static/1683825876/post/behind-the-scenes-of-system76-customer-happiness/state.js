5f8111950134e6cbaab297001bd4fcb6065fa59b
burden <burden@users.noreply.github.com>
2023-05-12 14:44:24 +0000

    deploy: 0796f0ddfb9b3664f1106ae02296bb5c9bfb2afb

--- _nuxt/static/1683825876/post/behind-the-scenes-of-system76-customer-happiness/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1683825876",layout:"default",error:a,serverRendered:true,routePath:"\u002Fpost\u002Fbehind-the-scenes-of-system76-customer-happiness",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
