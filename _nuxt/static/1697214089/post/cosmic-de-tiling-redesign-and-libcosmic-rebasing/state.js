19a8bb7d25cf759b2291070a9c2ddc933d8a2125
burden <burden@users.noreply.github.com>
2023-10-13 16:21:33 +0000

    deploy: 8c298d8a975600b6f3aa609cf6827d59a1e072e7

---
+++ _nuxt/static/1697214089/post/cosmic-de-tiling-redesign-and-libcosmic-rebasing/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1697214089",layout:"default",error:a,serverRendered:true,routePath:"\u002Fpost\u002Fcosmic-de-tiling-redesign-and-libcosmic-rebasing",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
