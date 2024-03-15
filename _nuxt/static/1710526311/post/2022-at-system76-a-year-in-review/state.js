7ee75a3f51bdbd088b656681ffe5546015e69e9c
macifell <macifell@users.noreply.github.com>
2024-03-15 18:16:29 +0000

    deploy: e90b4aecd5975f555b9808bcd15efd0c250c4387

--- _nuxt/static/1710526311/post/2022-at-system76-a-year-in-review/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1710526311",layout:"default",error:a,serverRendered:true,routePath:"\u002Fpost\u002F2022-at-system76-a-year-in-review",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ _nuxt/static/1710526583/post/engineering-an-improved-thelio-desktop-line-with-carl-richell/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1710526583",layout:"default",error:a,serverRendered:true,routePath:"\u002Fpost\u002Fengineering-an-improved-thelio-desktop-line-with-carl-richell",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
