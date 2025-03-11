338a274e37c4a7129ade3841e701624d9f7fcd74
burden <burden@users.noreply.github.com>
2025-03-11 14:03:07 +0000

    deploy: 36b90dc87811e09839c42fd5bdbc1bef3d9daf8e

--- _nuxt/static/1741701657/post/buyers-guide-pc-gaming/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1741701657",layout:"default",error:a,serverRendered:true,routePath:"\u002Fpost\u002Fbuyers-guide-pc-gaming",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
