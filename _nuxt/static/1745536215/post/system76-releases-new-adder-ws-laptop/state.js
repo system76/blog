48b5babd49394fb51abd863c8d9ff050d13e7aff
burden <burden@users.noreply.github.com>
2025-05-05 18:18:20 +0000

    deploy: 36b90dc87811e09839c42fd5bdbc1bef3d9daf8e

--- _nuxt/static/1745536215/post/system76-releases-new-adder-ws-laptop/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1745536215",layout:"default",error:a,serverRendered:true,routePath:"\u002Fpost\u002Fsystem76-releases-new-adder-ws-laptop",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
