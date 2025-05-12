3a06590b6beafb9a8c6f4cbb2acbe947f1877732
burden <burden@users.noreply.github.com>
2025-05-12 13:38:16 +0000

    deploy: 36b90dc87811e09839c42fd5bdbc1bef3d9daf8e

--- _nuxt/static/1746738685/post/celebrating-5-years-of-pop_os/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1746738685",layout:"default",error:a,serverRendered:true,routePath:"\u002Fpost\u002Fcelebrating-5-years-of-pop_os",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
